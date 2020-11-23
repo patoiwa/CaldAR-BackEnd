526c7e7142d320f1507f6561acec513b934a84ec
Martina Moser <mmoser.ar@gmail.com>
2020-11-23 12:11:14 -0300

    Created the function to get customers by Id

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -9,3 +9,13 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 app.get("/getAllCustomers", (req, res)=>{
     res.json(customers)
 });
+//getCustomerById
+app.get("/getCustomerById/:id", (req,res) =>{
+    const found = customers.filter (customer => customer.id === parseInt(req.params.id));
+    
+    if (found.length > 1){
+        res.json(found)
+    } else {
+        res.status(400).json({ msg: `No customer with the id of ${req.params.id}`})
+    }
+});
