285168c3cc5fdc5742cd794b5337444a9dee0ccc
Santiago Villa <santiagovillavega@gmail.com>
2020-11-23 12:22:39 -0300

    CALD-007: getBoilerTypeById request done

--- controllers/BoilerTypesController.js
+++ controllers/BoilerTypesController.js
@@ -8,4 +8,15 @@ app.get("/", (req, res)=>{
     res.json(boilerTypes)
 });
 
+// getBoilerTypeById
+app.get("/getBoilerTypeById/:id", (req, res) => {
+    const found = boilerTypes.some(boilerTypes => boilerTypes.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json(boilerTypes.filter(boilerTypes => boilerTypes.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ msg: `No boiler type with the id of ${req.params.id}`})
+    }
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
