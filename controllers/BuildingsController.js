3c04db9020df4eb19105f6631813ab47a08b973b
Martina Moser <mmoser.ar@gmail.com>
2020-11-23 12:40:31 -0300

    Correction of condition of sentence if on getBuildingById and getBuildingByAttribute

--- controllers/BuildingsController.js
+++ controllers/BuildingsController.js
@@ -1,30 +1,32 @@
 const express = require(`express`);
-const { find } = require("../../Express/Members");
 const app = express();
 const buildings = require ("../data/Buildings.json")
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// getBuildingsAll
-app.get("/", (req, res)=>{
+// getAllBuildings
+app.get("/getAllBuildings", (req, res)=>{
     res.json(buildings)
 });
+
 //getBuildingById
 app.get("/getBuildingById/:id", (req,res) =>{
     const found = buildings.filter (building => building.id === parseInt(req.params.id));
     
-    if (found){
+    if (found.length > 0){
         res.json(found)
     } else {
         res.status(400).json({ msg: `No building with the id of ${req.params.id}`})
     }
 });
+
+//getBuildingByAttribute
 //getBuildingByAddress
 app.get("/getBuildingByAddress/:address", (req,res) =>{
     const found = buildings.filter (building => building.address === String (req.params.address));
 
-    if (found){
+    if (found.length > 0){
         res.json(found);
     } else {
         res.status(400).json({ msg: `No building with the address of ${req.params.address}`})
@@ -35,32 +37,33 @@ app.get("/getBuildingByBoilersId/:boilersId", (req,res) =>{
     const boilersIdNumber = parseInt (req.params.boilersId);
     const found = buildings.filter(building => building.boilersId.includes(boilersIdNumber));
     
-    if (found){
+    if (found.length > 0){
         res.json(found);
     } else {
         res.status(400).json({ msg: `No building with the boiler ID of ${req.params.boilersId}`})
     }
 });
 //getBuildingByFullName
-app.get("/getBuildingByFullName/:fullName", (req,res) =>{
+app.get("/getBuildingsByFullName/:fullName", (req,res) =>{
     const found = buildings.filter (building => building.fullName === String (req.params.fullName));
 
-    if (found){
+    if (found.length > 0){
         res.json(found);
     } else {
         res.status(400).json({ msg: `No building with the name of ${req.params.fullName}`})
     }
 });
 //getBuildingByPhone
-app.get("/getBuildingByPhone/:phone", (req,res) =>{
+app.get("/getBuildingsByPhone/:phone", (req,res) =>{
     const found = buildings.filter (building => building.phone === String (req.params.phone));
 
-    if (found){
+    if (found.length > 0){
         res.json(found)
     } else {
         res.status(400).json({ msg: `No building with the phone of ${req.params.phone}`})
     }
 });
+
 //deleteBuildingById
 app.delete ("/:id", (req, res)=>{
     const found = buildings.some(building =>building.id === parseInt(req.params.id))
